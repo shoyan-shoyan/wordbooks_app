83bd5ffd6a1b2814b7724136bab06655890f4f7c
shoyan-shoyan <shlesson7@gmail.com>
2022-01-02 18:40:46 +0900

    profile edit auth

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -55,38 +55,44 @@ class UsersController extends Controller
     public function edit($id)
     {
         $user = User::findorFail($id);
-
+        if (\Auth::id() == $id) {
         return view('users.edit', compact('user')); 
+        }
+        return view('welcome');
     }
 
     public function update(ValidateRequest $request, $id)
     {
+        if (\Auth::check()) {
+            $user = User::findorFail($id);
 
-        $user = User::findorFail($id);
+            if (\Auth::id() == $id) {
+                if(!is_null($request['img_name'])){
+                    $imageFile = $request['img_name'];
+                    
+                    // 画像ファイルのサイズ確認
+                    $imgSize = filesize($imageFile);
 
-        if(!is_null($request['img_name'])){
-            $imageFile = $request['img_name'];
-            
-            // 画像ファイルのサイズ確認
-            $imgSize = filesize($imageFile);
+                    if($imgSize < 1100000){
 
-            if($imgSize < 1100000){
+                        $list = FileUploadServices::fileUpload($imageFile);
+                        list($extension, $fileData) = $list;
 
-                $list = FileUploadServices::fileUpload($imageFile);
-                list($extension, $fileData) = $list;
+                        $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
+                        $user->img_name = $data_url;
 
-                $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
-                $user->img_name = $data_url;
+                    }
+                    
+                }
+                
+                $user->self_introduction = $request->self_introduction;
 
+                $user->save();
+
+                return redirect()->route('users.show',['user' => $id]);
             }
-            
         }
-        
-        $user->self_introduction = $request->self_introduction;
-
-        $user->save();
-
-        return redirect()->route('users.show',['user' => $id]);
+        return view('welcome');
     }
 
     public function followings($id)
--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -11,9 +11,11 @@
             {{ $user -> self_introduction }}
         </div>
     </div>
+    @if (Auth::id() == $user->id)
     <div class="card-footer">
         {!! link_to_route('users.edit', '情報を編集', ['user' => $user->id],['class'=>'btn btn-outline-dark btn-sm fas fa-edit fa-2x']) !!}
     </div>
+    @endif
 </div>
 {{-- フォロー／アンフォローボタン --}}
 @include('user_follow.follow_button')
